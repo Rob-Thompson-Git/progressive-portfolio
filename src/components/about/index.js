eb03d956554527615ebfb9c0a056da969b561d65
RobThompson <rob.thomp15@gmail.com>
2022-12-29 19:43:24 -0600

    styled contact form

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -69,25 +69,3 @@ const About = () => {
 
 
 export default About;
-
-    {/* //     <div name='about' id="about" className='w-full h-full bg-slate-900 text-gray-50 py-1'>
-    //      <div className='flex flex-col justify-items-center items-center py-[50px]'>
-    //          <div className='max-w-[1000px] grid grid-cols-2 gap-8 '>
-    //                 <div className=' pb-8 '>
-    //                     <h1 className='text-4xl font-bold inline border-b-4 border-blue-400'>
-    //                      About Me
-    //                     </h1>
-    //                 </div>
-    //                 </div>
-    //                 <div className=' grid sm:grid-cols-2 gap-8 '>
-    //                     <div className='place-self-center md:place-self-center text-align-center w-3/5'>
-    //                         <img className=' rounded-full place-self-center' src={img1} alt="rob thompson web development" />
-    //                     </div>
-    //                     <div className='border-t-8 text-center place-self-center w-[75%] border-t-red-500  sm:border-t-0 sm:border-l-8 sm:border-l-red-500 
-    //                     px-10'>
-    //                         <h2 className='text-4xl font-bold pt-6 text-start  text-blue-400'>Hi, thanks for stopping by! Please take a look around.</h2>
-    //                         <p className='text-gray-50 text-2xl text-start pt-2'>On the front-end, I am proficient in HTML, handlebars, CSS (and frameworks: bootstrap, UIKit and Bulma), JavaScript (both synchronous and asynchronous) and JQuery. Much of my experience on the front-end includes creating web applications revolved around data</p>
-    //              </div>
-    //          </div>
-    //      </div>
-    //  </div> */}
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -38,29 +38,29 @@ const Contact = () => {
   return (
   <div name="contact" id="contact" className='w-full h-full bg-slate-900 grid grid-cols-1 pt-20 pb-40'>
 <div className='w-3/4 place-self-center'>
-    <div  className="relative rounded-2xl border-8 px-10"  >
-      <div className="absolute inset-0 bg-gray-50 ">
-        <div className="absolute inset-y-0 left-0 w-1/2  bg-gray-50" />
+    <div className="relative rounded-2xl border-8 hover:border-sky-300 px-10">
+      <div className="absolute inset-0 bg-gray-50 rounded-md">
+        <div className="absolute inset-y-0 left-0 w-1/2  bg-gray-50 rounded-md" />
       </div>
       <div className="relative mx-auto max-w-7xl lg:grid lg:grid-cols-5">
         <div className="bg-gray-100 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-10 xl:pr-12 rounded-2xl">
           <div className="mx-auto max-w-lg rounded-2xl">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Contact Me</h2>
-            <p className="mt-2 text-lg leading-6 text-gray-500">
-              Thank you for viewing.
+            <h2 className="text-2xl font-bold tracking-tight text-slate-800 sm:text-3xl">Contact Me</h2>
+            <p className="mt-2 text-lg leading-6 text-slate-800">
+              Thank you for viewing! If you would like to contact me, please fill out the form and I will get back with you shortly.
             </p>
-            <dl className="mt-8 text-base text-gray-500">
+            <dl className="mt-8 text-base text-slate-900">
               <div className="mt-6">
                 <dt className="sr-only">Phone number</dt>
                 <dd className="flex">
-                  <PhoneIcon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                  <PhoneIcon className="h-6 w-6 flex-shrink-0 text-sky-400" aria-hidden="true" />
                   <span className="ml-3">+1 (614) 440-7141</span>
                 </dd>
               </div>
               <div className="mt-3">
                 <dt className="sr-only">Email</dt>
                 <dd className="flex">
-                  <EnvelopeIcon className="h-6 w-6 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                  <EnvelopeIcon className="h-6 w-6 flex-shrink-0 text-sky-400" aria-hidden="true" />
                   <span className="ml-3">rob.thomp15@gmail.com</span>
                 </dd>
               </div>
@@ -80,7 +80,7 @@ const Contact = () => {
                   name="from_name"
                   id="full-name"
                   autoComplete="name"
-                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-sky-300 focus:ring-sky-300"
                   placeholder="Full name"
                 />
               </div>
@@ -93,7 +93,7 @@ const Contact = () => {
                   name="from_email"
                   type="email"
                   autoComplete="email"
-                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-sky-300 focus:ring-sky-300"
                   placeholder="Email"
                 />
               </div>
@@ -105,7 +105,7 @@ const Contact = () => {
                   id="message"
                   name="message"
                   rows={4}
-                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                  className="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-sky-300 focus:ring-sky-300"
                   placeholder="Message"
                   defaultValue={''}
                 />
@@ -113,7 +113,7 @@ const Contact = () => {
               <div className='grid grid-cols-2 gap-20'>
                 <button
                   type="submit"
-                  className="rounded-md border border-transparent bg-indigo-600 py-3 px-6 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="rounded-md border border-transparent bg-sky-400 py-3 px-6 text-base font-medium text-white shadow-sm hover:bg-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-300 focus:ring-offset-2"
                 >
                   Send
                 </button>
